90aab48ba3f45e5913b607ece119974c01eb4776
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2025-01-28 11:35:33 +0100

    Add a scipt folder and movecustom.js into it

--- scripts/custom.js
+++ scripts/custom.js
@@ -6,7 +6,7 @@ const works = [
     altToImage: "Project work",
     hoverText: "Project 1",
     linkToFile:
-      "works/2022/PLOT 679, CAD ZONE C04, DAPE DISTRICT, FCT ABUJA (1).pdf",
+      "works/2022/PLOT 679, CAD ZONE C04, DAPE DISTRICT, FCT ABUJA (1).pdf"
   },
   {
     year: "2022",
@@ -15,71 +15,72 @@ const works = [
     altToImage: "Project work",
     hoverText: "Project 2",
     linkToFile:
-      "works/2022/FEDERAL INSTITUTE OF INDUSTRIAL RESEARCH financial bid 2022.pdf",
+      "works/2022/FEDERAL INSTITUTE OF INDUSTRIAL RESEARCH financial bid 2022.pdf"
   },
   {
     year: "2023",
     image: "works/2023/EKITI MOSQUE.png",
     altToImage: "Project work",
     hoverText: "Project 1",
-    linkToFile: "works/2023/EKITI MOSQUE.pdf",
+    linkToFile: "works/2023/EKITI MOSQUE.pdf"
   },
   {
     year: "2023",
     image: "works/2023/Kaduna Plot 8 Angwa Rimi GRA 4Structural.jpg",
     altToImage: "Project work",
     hoverText: "Project 2",
-    linkToFile: "works/2023/Kaduna Plot 8 Angwa Rimi GRA 4Structural.pdf",
+    linkToFile: "works/2023/Kaduna Plot 8 Angwa Rimi GRA 4Structural.pdf"
   },
   {
     year: "2023",
     image: "works/2023/TYPE 1,2,3,4&5 CONSTRUCTION 14-11-2023.png",
     altToImage: "Project work",
     hoverText: "Project 3",
-    linkToFile: "works/2023/TYPE 1,2,3,4&5 CONSTRUCTION 14-11-2023.pdf",
+    linkToFile: "works/2023/TYPE 1,2,3,4&5 CONSTRUCTION 14-11-2023.pdf"
   },
   {
     year: "2024",
     image: "works/2024/ARCHITECTURAL DRAWING PETROL SERVICE STATION.png",
     altToImage: "Project work",
     hoverText: "Project 1",
-    linkToFile: "works/2024/ARCHITECTURAL DRAWING PETROL SERVICE STATION.pdf",
+    linkToFile: "works/2024/ARCHITECTURAL DRAWING PETROL SERVICE STATION.pdf"
   },
   {
     year: "2024",
     image: "works/2024/KAURA 332 STRUCT 2024.png",
     altToImage: "Project work",
     hoverText: "Project 2",
-    linkToFile: "works/2024/KAURA 332 STRUCT 2024.pdf",
+    linkToFile: "works/2024/KAURA 332 STRUCT 2024.pdf"
   },
   {
     year: "2024",
     image: "works/2024/Print _V O N_LUGBE CAD E29_Arch Drwg.png",
     altToImage: "Project work",
     hoverText: "Project 3",
-    linkToFile: "works/2024/Print _V O N_LUGBE CAD E29_Arch Drwg.pdf",
+    linkToFile: "works/2024/Print _V O N_LUGBE CAD E29_Arch Drwg.pdf"
   },
   {
     year: "2024",
     image: "works/2024/Wuse II Plot 1075 Arch Drwg.png",
     altToImage: "Project work",
     hoverText: "Project 4",
-    linkToFile: "works/2024/Wuse II Plot 1075 Arch Drwg.pdf",
+    linkToFile: "works/2024/Wuse II Plot 1075 Arch Drwg.pdf"
   },
   {
     year: "Civil works",
     image: "works/civil works/01- FOUND & PILE CAP-new.png",
     altToImage: "Project work",
     hoverText: "Project 1",
-    linkToFile: "works/civil works/01- FOUND & PILE CAP-new.pdf",
+    linkToFile: "works/civil works/01- FOUND & PILE CAP-new.pdf"
   },
   {
     year: "Civil works",
     image: "works/civil works/Lokogoma Estate Roads  Design23_01_2019 (3).png",
     altToImage: "Project work",
     hoverText: "Project 1",
-    linkToFile: "works/civil works/Lokogoma Estate Roads  Design23_01_2019 (3).pdf",
-  },
+    linkToFile:
+      "works/civil works/Lokogoma Estate Roads  Design23_01_2019 (3).pdf"
+  }
 ];
 
 const jsWorks = document.querySelector(".js-works");
